0e0be5eb2d8f202e7ed12ba41898638dbbac8cfa
Argus-66 <jqghebri@gmail.com>
2025-02-10 01:45:10 +0530

    commit

---
+++ 2_patterns/pattern1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+// To make this pattern 
+// *****
+// *****
+// *****
+// *****
+// *****
+
+void pattern(int n, int m) {
+    fori(n){
+        forj(m){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+
+int main() {
+    int row, col;
+    cout << "Enter the number of rows: ";
+    cin >> row;
+    cout << "Enter the number of columns: ";
+    cin >> col;
+
+    pattern(row, col);
+
+    return 0;
+}
