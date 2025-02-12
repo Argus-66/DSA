2e7545fa87b5bd1c29dcf82ad6c4c8a686a626e2
Argus-66 <jqghebri@gmail.com>
2025-02-12 13:37:25 +0530

    commit

--- 3_STL/2_Containers/1_vectors.cpp
+++ 3_STL/2_Containers/1_vectors.cpp
@@ -68,8 +68,91 @@ void vectorIterator(){
 
 
     // NEVER USED BUT GOOD TO KNOE
-    vector<int>::reverse_iterator name3 = v.rend();     //
-    vector<int>::reverse_iterator name4 = v.rbegin();   //
+    vector<int>::reverse_iterator name3 = v.rend();     // reversing the end, just before the first element
+    // v.rend() returns a reverse iterator that points to one element before the first element of the vector.
+    cout << *(name3) << endl; //  points to one element before the first element. Dereferencing v.rend() is not valid (undefined behavior). In your case, it printed 0, but you shouldn’t rely on that value.
+    vector<int>::reverse_iterator name4 = v.rbegin();   //  reversing the start, pointing to the first element
+    // v.rbegin() returns a reverse iterator that points to the last element of the vector.
+    cout << *(name4) << endl; // v.rbegin() points to the last element (which is 5). So *(v.rbegin()) gives you 5.
+
+    for(auto rit = v.rbegin(); rit != v.rend(); ++rit) {
+        cout << *rit << " "; // This will print: 5 4 3 2 1
+    }
+    cout << endl;
+
+    // Accessing elements using [] and at()
+    cout<< v[0] << " " << v.at(0) << endl; // This will print: 1 1 because they both are valid ways to access the first element.
+    cout << "Back: " << v.back() << " " << "Front: " << v.front() << endl; // This will print: 5 1
+
+    // Loop
+    for(vector<int>::iterator it = v.begin(); it!= v.end(); ++it) {
+        cout<< *it << " "; // This will print: 1 2 3 4 5
+    }
+    cout << endl;
+
+    // Shortcut
+    // auto keyword in C++11 and later allows us to automatically deduce the type of an iterator. no need to specify the type explicitly.?/?
+    cout << "Shortter Method: ";
+    for(auto it = v.begin(); it!= v.end(); ++it) {
+        cout<< *it << " "; // This will print: 1 2 3 4 5
+    }
+    cout << endl;
+
+    cout<< "Using Range-based for loop: ";  // This is more modern and efficient. It automatically handles iterator increment and decrements.
+    for(auto it : v){
+        cout<< it << " "; // This will print: 1 2 3 4 5
+    }
+    cout << endl;
+
+    //erasing or deleting in a vector
+
+    v.erase(v.begin()); // remove the first element
+    cout << "After erasing the first element: ";
+    for(auto it : v){
+        cout<< it << " "; // This will print: 2 3 4 5
+    }
+    cout<< endl;
+
+    v.erase(v.begin()+1, v.begin()+3); // remove elements from index 1 to 2 (inclusive)
+    cout << "After erasing elements from index 1 to 2: ";
+    for(auto it : v){
+        cout<< it << " "; // This will print: 3 5
+    }
+    cout<< endl;
+
+    // Insert Function
+
+    vector<int> v5(2,100); // {100, 100}
+    v5.insert(v5.begin(), 300); // insert 10 at the beginning of the vector
+    cout << "After inserting 10 at the beginning: ";
+    for(auto it : v5){
+        cout<< it << " "; // This will print: 300 100 100
+    }
+    cout<< endl;
+    v5.insert(v5.begin() + 1, 2, 10);     // 2  is for how many times we want to insert 10 at index 1
+    cout << "After inserting 10 at index 1: ";
+    for(auto it : v5){
+        cout<< it << " "; // This will print: 300 10 10 100 100
+    }
+    cout<< endl;
+
+    cout << v5.size() << "This is the size of the vector." << endl;
+
+    v5.pop_back(); // remove the last element
+    cout << "After popping the last element: ";
+    for(auto it : v5){
+        cout<< it << " "; // This will print: 300 10 10 100
+    }
+    cout<< endl;
+
+    v5.swap(v); // swap v5 with v
+    cout << "After swapping v5 with v: ";
+    for(auto it : v){
+        cout<< it << " "; // This will print: 10 10 100 100
+    }
+    cout<< endl;
+
+    cout << v.empty() << endl;  // This will empty the vector and return 0 if it is empty and 1 otherwise.
 }
 
 int main(){
