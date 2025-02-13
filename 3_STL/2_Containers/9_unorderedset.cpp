07b35d2eedf4ebc286ec1421b91ba171897b9aba
Argus-66 <jqghebri@gmail.com>
2025-02-13 20:23:55 +0530

    commit

---
+++ 3_STL/2_Containers/9_unorderedset.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+// Unordered Set
+// UNIQUE but NOT ORDERED
+// lower bound and upper bound function does not work with unordered_set
+// Time Complexity: O(1) for insert, erase, find, and count operations
+// unordered_set is faster than set for lookups, but slower for insertions and deletions.
+// therefore faster in most cases than set.
+// Worst case time complexity is O(n) for insertions and deletions. (once in a blue moon)
+
+
+
+
+
+int main() {
+
+
+    return 0;
+}
