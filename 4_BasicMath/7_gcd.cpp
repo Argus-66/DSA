6d4111d9b56e458f294deb597ce955925e604bd8
Argus-66 <jqghebri@gmail.com>
2025-02-19 17:52:00 +0530

    commit

---
+++ 4_BasicMath/7_gcd.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+// HFC is also known as GCD
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << "GCD: ";  // Function to calculate GCD
+    cout << __gcd(a, b) << endl;
+    cout << "LCM: ";  // Function to calculate LCM
+    cout << lcm(a, b) << endl;
+
+    return 0;
+}
