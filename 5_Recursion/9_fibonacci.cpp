762fa6a4d811edcae4228801bd5b15e9f46b90c9
Argus-66 <jqghebri@gmail.com>
2025-02-27 23:21:02 +0530

    commit

---
+++ 5_Recursion/9_fibonacci.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+    public:
+        int fib(int n) {
+            if(n<=1) return n;
+
+            int last = fib(n-1);
+            int slast = fib(n-2);
+
+            return last+ slast;
+        }
+    };
+
+
+int main() {
+    Solution s;
+    int n;
+    cin >> n;
+    cout << s.fib(n) << endl;
+
+    return 0;
+
+
+}
