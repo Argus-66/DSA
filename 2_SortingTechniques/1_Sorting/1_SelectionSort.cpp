b260c0c507f6db3aeb10bcc31efd8922ce776db7
Argus-66 <jqghebri@gmail.com>
2025-03-04 07:00:42 +0530

    commit

--- 2_SortingTechniques/1_Sorting/1_SelectionSort.cpp
+++ 2_SortingTechniques/1_Sorting/1_SelectionSort.cpp
@@ -62,10 +62,17 @@ using namespace std;
 
 void selection_sort(int arr[], int n){
     for(int i=0; i<=n-2;i++){
-        int min=i;
-        for(int j=i;j<=n-1;i++){
-            
+        int mini=i;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini=j;
+            }
         }
+        // int temp = arr[mini];
+        // arr[mini] = arr[i];
+        // arr[i] = temp;
+
+        swap(arr[i], arr[mini]);
     }
 }
 
