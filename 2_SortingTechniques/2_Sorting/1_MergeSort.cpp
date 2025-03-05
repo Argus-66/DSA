9f0834397eddf6469909d6a49821d2ec39445205
Argus-66 <jqghebri@gmail.com>
2025-03-06 00:20:30 +0530

    commit

--- 2_SortingTechniques/2_Sorting/1_MergeSort.cpp
+++ 2_SortingTechniques/2_Sorting/1_MergeSort.cpp
@@ -52,12 +52,10 @@ using namespace std;
 *    [1,     3]     [2]     [4]  [1]        [5]  [2]    [6]  [4]
 
 ?    2nd layer
-*    [1,     3]    [2]     [1,    4]        [2,     5]     [4,    6]
 
-?   3rd layer
 *    [1,    2,    3]       [1,    4]        [2,     5]     [4,    6]
 
-?   4th layer
+?   3rd layer
 *    [1,    1,    2,    3,    4]       [2,    4,    5,    6]
 
 ?   Final Layer
