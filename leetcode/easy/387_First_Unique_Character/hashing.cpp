f8df0d5c26a88f0a3af577c6192e42c15e3ce14f
Argus-66 <jqghebri@gmail.com>
2025-07-17 17:34:13 +0530

    commit

--- leetcode/easy/387_First_Unique_Character/hashing.cpp
+++ leetcode/easy/387_First_Unique_Character/hashing.cpp
@@ -17,8 +17,18 @@ using namespace std;
 
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        
+    int firstUniqChar(string s) {
+        unordered_map<int,int>freq;
+
+        for (char c : s) {
+            freq[c]++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
     }
 };
 
