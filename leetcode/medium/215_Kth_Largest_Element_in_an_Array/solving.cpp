6ee2014a555ade591446522cfa806d5810294b4c
Argus-66 <jqghebri@gmail.com>
2025-08-03 14:45:39 +0530

    solved 225

---
+++ leetcode/medium/215_Kth_Largest_Element_in_an_Array/solving.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for (int num : nums) {
+            minHeap.push(num);
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
+
+
+
+
+int main() {
+    
+    return 0;
+}
