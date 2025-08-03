52747b4c08556d55d71b5d9c2426fac36df41e11
Argus-66 <jqghebri@gmail.com>
2025-08-03 15:26:49 +0530

    solved 1752

---
+++ 3_SolveProblemsonArrays/1_Easy/3_Check_if_the_array_is_sorted.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+int isSorted(int n, vector<int>arr){
+    for(int i=1;i<n;i++){
+        if(arr[i]>=arr[i-1]){
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+int main() {
+    
+    return 0;
+}
