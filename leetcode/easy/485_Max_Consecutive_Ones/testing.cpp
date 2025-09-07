e17da09bafbbea6d9da0f3e62035cd82bde2a2f3
Argus-66 <jqghebri@gmail.com>
2025-09-08 02:03:08 +0530

    Solved 485

---
+++ leetcode/easy/485_Max_Consecutive_Ones/testing.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int count =0;
+        int max =0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+                if(count>max){
+                    max = count;
+                }
+            }
+            else{
+                count=0;
+            }
+        }
+        return max;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
