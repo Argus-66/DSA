4fc04f5b3084b2aa6c35e51b677d26ff5729e709
Argus-66 <jqghebri@gmail.com>
2025-09-08 20:06:36 +0530

    commit

--- 3_SolveProblemsonArrays/1_Easy/14_longest_Pos_Neg.cpp
+++ 3_SolveProblemsonArrays/1_Easy/14_longest_Pos_Neg.cpp
@@ -14,28 +14,34 @@ using namespace std;
 #define set                      set<ll>
 #define mod                      1000000007
 
-int getLongestSubarray(vector<int>& a, long long k) {
+int getLongestSubarray(vector<int>& a, int k) {
     int n = a.size(); // size of the array.
 
-    int left = 0, right = 0; // 2 pointers
-    long long sum = a[0];
+    map<int, int> preSumMap;
+    int sum = 0;
     int maxLen = 0;
-    while (right < n) {
-        // if sum > k, reduce the subarray from left
-        // until sum becomes less or equal to k:
-        while (left <= right && sum > k) {
-            sum -= a[left];
-            left++;
-        }
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
 
-        // if sum = k, update the maxLen i.e. answer:
+        // if the sum = k, update the maxLen:
         if (sum == k) {
-            maxLen = max(maxLen, right - left + 1);
+            maxLen = max(maxLen, i + 1);
         }
 
-        // Move forward thw right pointer:
-        right++;
-        if (right < n) sum += a[right];
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
     }
 
     return maxLen;
