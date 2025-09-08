e3c7ce04bfa4ff7beaddae04f244e66d521bdeeb
Argus-66 <jqghebri@gmail.com>
2025-09-09 03:24:49 +0530

    Solved 169

---
+++ leetcode/easy/169._Majority_Element/testing.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(int i : nums){
+            mp[i]++;
+            if(mp[i]>n/2){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
