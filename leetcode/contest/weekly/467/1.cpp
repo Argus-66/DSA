18df56fc6f84c49b03d7b4eaa5fab8472ec8c85a
Argus-66 <jqghebri@gmail.com>
2025-09-14 22:24:46 +0530

    COntest 467

---
+++ leetcode/contest/weekly/467/1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int ans = INT_MAX;
+        for(auto task: tasks){
+            int s = task[0];
+            int d = task[1];
+            int sum = s + d;
+            ans=min(ans,sum);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
