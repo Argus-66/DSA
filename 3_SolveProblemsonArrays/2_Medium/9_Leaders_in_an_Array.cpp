1f4f5ed6341ce02b4da828eb3efd51272715d7ee
Argus-66 <jqghebri@gmail.com>
2025-09-15 17:10:10 +0530

    commit

--- 3_SolveProblemsonArrays/2_Medium/9_Leaders_in_an_Array.cpp
+++ 3_SolveProblemsonArrays/2_Medium/9_Leaders_in_an_Array.cpp
@@ -18,17 +18,14 @@ using namespace std;
 vector<int> printLeaders(int arr[], int n) {
     vector<int> ans;
 
- // Last element of an array is always a leader,
- // push into ans array.
-    int max = arr[n - 1];
+    int maxi = arr[n - 1];
     ans.push_back(arr[n-1]);
 
-  // Start checking from the end whether a number is greater
-  // than max no. from right, hence leader.
+
     for (int i = n - 2; i >= 0; i--)
-        if (arr[i] > max) {
+        if (arr[i] > maxi) {
             ans.push_back(arr[i]);
-            max = arr[i];
+            maxi = arr[i];
         }
     return ans;
 }
