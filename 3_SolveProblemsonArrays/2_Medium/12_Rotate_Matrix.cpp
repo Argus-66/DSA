6634c490c6e2e6142c4baad92aea48e0462d4f63
Argus-66 <jqghebri@gmail.com>
2025-09-18 01:42:10 +0530

    commit

---
+++ 3_SolveProblemsonArrays/2_Medium/12_Rotate_Matrix.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+    }
+};
+
+int main() {
+    
+    return 0;
+}
