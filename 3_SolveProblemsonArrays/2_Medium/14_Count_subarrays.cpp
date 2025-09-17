a98c889a078bbb410944f911acc87451b2c6d713
Argus-66 <jqghebri@gmail.com>
2025-09-18 02:54:15 +0530

    commit

---
+++ 3_SolveProblemsonArrays/2_Medium/14_Count_subarrays.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0, count = 0;
+        for (int x : nums) {
+            sum += x;
+            if (mp.find(sum - k) != mp.end()) {
+                count += mp[sum - k];
+            }
+            mp[sum]++;
+        }
+        return count;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
