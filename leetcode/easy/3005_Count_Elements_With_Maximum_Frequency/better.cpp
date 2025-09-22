b17ec15c24083c91d9054dec4a47ecde729a29bc
Argus-66 <jqghebri@gmail.com>
2025-09-23 03:49:27 +0530

    Solved 3005

---
+++ leetcode/easy/3005_Count_Elements_With_Maximum_Frequency/better.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int,int>hash;
+        int mf=0;
+        int ans=0;
+
+        for(int a: nums){
+            int freq=++hash[a];
+
+            if(freq>mf){
+                mf=freq;
+                ans=freq;
+            }
+            else if(freq==mf){
+                ans+=freq;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
