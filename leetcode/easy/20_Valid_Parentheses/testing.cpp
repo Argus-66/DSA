d0a8bd0b4f8199ef602c39b0b63dd5611e866c0f
Argus-66 <jqghebri@gmail.com>
2025-09-25 02:36:11 +0530

    Solved 20

---
+++ leetcode/easy/20_Valid_Parentheses/testing.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                       long long int
+#define test                     ll t; cin>>t; while(t--)
+#define ld                       long double
+#define tolower(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define toupper(s1)              transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define PI                       3.1415926535897932384626433832795l
+#define fori(n) for (int i = 0; i < n; i++)     //fori(n) {}
+#define forj(n) for (int j = 0; j < n; j++)
+#define fork(n) for (int k = 0; k < n; k++)
+#define pb                       push_back
+#define set                      set<ll>
+#define map                      map<ll,ll>
+#define mod                      1000000007
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            } 
+            else {
+                if(st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                if((c==')' && top!='(') ||
+                    (c=='}' && top!='{') ||
+                    (c==']' && top!='[')){
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
